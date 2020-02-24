6b17f95057532116dad8bf0803aba2ab1f0290ae
sophiaevanisko <sophiajevanisko@gmail.com>
2020-02-24 08:53:00 -0500

    Initial commit

---
+++ app/src/main/java/com/evaniskosophia/layoutslab04/MainActivity.java
@@ -0,0 +1,22 @@
+package com.evaniskosophia.layoutslab04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.corner_layout); //finished
+        //setContentView(R.layout.top_layout);  //finished
+        //setContentView(R.layout.guideline_layout); //finished
+        //setContentView(R.layout.overlapping_layout); //finished
+        //setContentView(R.layout.scroll_vertical_layout); //finished
+        //setContentView(R.layout.scroll_horizontal_layout); //finished
+       // setContentView(R.layout.advanced_layout); //finished
+    }
+}
